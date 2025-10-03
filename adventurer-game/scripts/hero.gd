fb2951899fa5e6f61764186546f0f7ba8470946f
Taylor <96559199+taylorlosch@users.noreply.github.com>
2025-10-03 17:22:57 -0400

    added new adventure types

--- adventurer-game/scripts/hero.gd
+++ adventurer-game/scripts/hero.gd
@@ -1,5 +1,16 @@
 extends Area2D
 
+
+var hero_sprites = [
+	preload("res://sprites/hero_warrior.tres"),
+	preload("res://sprites/hero_thief.tres"),
+	preload("res://sprites/hero_monk.tres"),
+	preload("res://sprites/hero_mage.tres"),
+	preload("res://sprites/hero_broom.tres"),
+	preload("res://sprites/hero_archer.tres"),
+]
+
+
 var spirit_scene = preload("res://scenes/spirit.tscn")
 var target_position: Vector2 = Vector2.ZERO
 var speed: float = 50.0
@@ -10,11 +21,11 @@ var bob_amount: float = 0.0
 var bob_offset: float = 0.0  
 
 func _ready() -> void:
+	$AnimatedSprite2D.sprite_frames = hero_sprites.pick_random()
 	bob_speed = randf_range(0.01, 0.02) 
 	bob_amount = randf_range(1.5, 3.0)  
 	bob_offset = randf_range(0.0, TAU)  
 	
-	# Optional: Random color tint
 	$AnimatedSprite2D.modulate = Color(
 		randf_range(0.8, 1.0),
 		randf_range(0.8, 1.0), 
